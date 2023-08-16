dd6d1467eab61ef720f50cd4cab2297a533395c4
Rhishabh Suneeth <rhishabhsuneeth@gmail.com>
2023-08-16 21:10:58 +0530

    fixed redirect bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import TopBar from './components/TopBar'
 import SideBar from './components/SideBar'
 import './App.css'
 import Home from './pages/home/Home'
-import { Outlet } from 'react-router-dom'
-import { redirect } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
 const App = () => {
-  const admin=JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.isAdmin;
-  useEffect(()=>{
-    if(!admin) return <redirect to='/login'/>
-  },[admin])
+  const user=JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser;
+  const admin=user?user.isAdmin: false;
+  if(!admin) return <Navigate to='/login'/>
   return (
     <div>
       <TopBar/>
